ace21fd56cd52cd6bcec06804eff652aa0529618
Rongsheng Xu <xurongsheng2010@gmail.com>
2015-12-04 23:32:12 -0600

    Location with radius

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,4 @@
-__author__ = 'rongshengxu'
+# __author__ = 'rongshengxu'
 from google.appengine.api import users
 
 import webapp2
@@ -10,21 +10,21 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-class LoginPage(webapp2.RequestHandler):
+class MainPage(webapp2.RequestHandler):
     def get(self):
-        if users.get_current_user():
-            self.redirect('/manage')
-        else:
-            url = users.create_login_url(self.request.url)
-            url_linktext = 'Sign in with Google'
-            template_values = {
-                'url' : url,
-                'url_linktext': url_linktext
-            }
-            template = JINJA_ENVIRONMENT.get_template('templates/login.html')
-            self.response.write(template.render(template_values))
+        # if users.get_current_user():
+        #     self.redirect('/manage')
+        # else:
+        #     url = users.create_login_url(self.request.url)
+        #     url_linktext = 'Sign in with Google'
+            # template_values = {
+            #     'url' : url,
+            #     'url_linktext': url_linktext
+            # }
+        template = JINJA_ENVIRONMENT.get_template('templates/test.html')
+        self.response.write(template.render())
 
 app = webapp2.WSGIApplication([
-    ('/', LoginPage),
+    ('/', MainPage),
 ], debug=True)
 
