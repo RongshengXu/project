7e1f3747a8ae9a522651f39e2a69fdb240257e91
xuanyang15 <yangxuanemail@gmail.com>
2015-12-06 10:46:29 -0600

    Finish location page v1

--- Backend/LocationHandler.py
+++ Backend/LocationHandler.py
@@ -1,5 +1,7 @@
 __author__ = 'rongshengxu'
 
+from google.appengine.api import users
+
 import webapp2
 import os
 import jinja2
@@ -11,8 +13,18 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 class LocationPage(webapp2.RequestHandler):
     def get(self):
+        user = users.get_current_user()
+        url = users.create_logout_url(self.request.url)
+        url_linktext = 'Logout'
+
+        template_values = {
+                'user': user,
+                'url': url,
+                'url_linktext': url_linktext,
+            }
+
         template = JINJA_ENVIRONMENT.get_template('templates/location.html')
-        self.response.write(template.render())
+        self.response.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([
     ('/location', LocationPage)
